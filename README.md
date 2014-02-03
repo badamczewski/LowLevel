fd87742ae973ff911c09492f6019a064387c9d97
Bartosz Adamczewski <bartoszadamczewski@gmail.com>
2014-02-04 00:15:02 +0100

    Initial Commit
    + Added Unsafe class that can do Off Heap allocation.
    + Added initial project structure.

---
+++ README.md
@@ -0,0 +1,5 @@
+# Low Level NET 
+
+NOTE: This project as of now is in hevy developement and cannot be even considered alpha.
+
+This is a library that exposes low level methods for some serious work.
---
+++ src/LowLevelNET/Unsafe.cs
@@ -0,0 +1,150 @@
+﻿#region Licence
+/*
+Copyright (c) 2011-2014 Contributors as noted in the AUTHORS file
+
+This file is part of LowLevelNET.
+
+LightString is free software; you can redistribute it and/or modify it under
+the terms of the GNU Lesser General Public License as published by
+the Free Software Foundation; either version 3 of the License, or
+(at your option) any later version.
+
+SCPM is distributed WITHOUT WARRANTY OF ANY KIND,
+EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+OTHER DEALINGS IN THE SOFTWARE.
+
+You should have received a copy of the GNU Lesser General Public License
+along with this program. If not, see <http://www.gnu.org/licenses/>.
+*/
+#endregion
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Runtime.InteropServices;
+using System.Diagnostics;
+
+namespace LowLevelNET
+{
+    public class Unsafe
+    {
+        public bool Is64Bit()
+        {
+            bool is64BitProcess = (IntPtr.Size == 8);
+
+            IntPtr handle = Process.GetCurrentProcess().Handle;
+
+            bool retVal;
+            IsWow64Process(handle, out retVal);
+
+            bool is64BitOperatingSystem = is64BitProcess || retVal;
+
+            return is64BitOperatingSystem;
+        }
+
+        public unsafe byte* Allocate(uint len)
+        {
+            SYSTEM_INFO sysinfo = new SYSTEM_INFO();
+            IntPtr handle = Process.GetCurrentProcess().Handle;
+
+            bool retVal;
+            IsWow64Process(handle, out retVal);
+
+            if (retVal == true)
+            {
+                GetNativeSystemInfo(ref sysinfo);
+            }
+            else
+            {
+                GetSystemInfo(ref sysinfo);
+            }
+
+            if (sysinfo.dwAllocationGranularity > len)
+                throw new ArgumentException(string.Format("The len argument is to small the minimum allocation size is {0}", sysinfo.dwAllocationGranularity.ToString()));
+
+            IntPtr ptr = VirtualAllocEx(handle, IntPtr.Zero, len, AllocationType.Commit, MemoryProtection.ReadWrite);
+
+            return (byte*)ptr;
+        }
+
+        [DllImport("kernel32.dll")]
+        private static extern void GetSystemInfo([MarshalAs(UnmanagedType.Struct)] ref SYSTEM_INFO lpSystemInfo);
+
+        [DllImport("kernel32.dll")]
+        private static extern void GetNativeSystemInfo([MarshalAs(UnmanagedType.Struct)] ref SYSTEM_INFO lpSystemInfo);
+
+        [DllImport("kernel32.dll", SetLastError = true, ExactSpelling = true)]
+        private static extern IntPtr VirtualAllocEx(IntPtr hProcess, IntPtr lpAddress,
+           uint dwSize, AllocationType flAllocationType, MemoryProtection flProtect);
+
+        [DllImport("kernel32.dll", SetLastError = true, CallingConvention = CallingConvention.Winapi)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        private static extern bool IsWow64Process(
+            [In] IntPtr hProcess,
+            [Out] out bool wow64Process
+        );
+
+        [StructLayout(LayoutKind.Sequential)]
+        struct SYSTEM_INFO
+        {
+            internal _PROCESSOR_INFO_UNION uProcessorInfo;
+            public uint dwPageSize;
+            public IntPtr lpMinimumApplicationAddress;
+            public IntPtr lpMaximumApplicationAddress;
+            public IntPtr dwActiveProcessorMask;
+            public uint dwNumberOfProcessors;
+            public uint dwProcessorType;
+            public uint dwAllocationGranularity;
+            public ushort dwProcessorLevel;
+            public ushort dwProcessorRevision;
+        }
+
+        [StructLayout(LayoutKind.Explicit)]
+        struct _PROCESSOR_INFO_UNION
+        {
+            [FieldOffset(0)]
+            internal uint dwOemId;
+            [FieldOffset(0)]
+            internal ushort wProcessorArchitecture;
+            [FieldOffset(2)]
+            internal ushort wReserved;
+        }
+
+        [Flags]
+        enum AllocationType
+        {
+            Commit = 0x1000,
+            Reserve = 0x2000,
+            Decommit = 0x4000,
+            Release = 0x8000,
+            Reset = 0x80000,
+            Physical = 0x400000,
+            TopDown = 0x100000,
+            WriteWatch = 0x200000,
+            LargePages = 0x20000000
+        }
+
+        [Flags]
+        enum MemoryProtection
+        {
+            Execute = 0x10,
+            ExecuteRead = 0x20,
+            ExecuteReadWrite = 0x40,
+            ExecuteWriteCopy = 0x80,
+            NoAccess = 0x01,
+            ReadOnly = 0x02,
+            ReadWrite = 0x04,
+            WriteCopy = 0x08,
+            GuardModifierflag = 0x100,
+            NoCacheModifierflag = 0x200,
+            WriteCombineModifierflag = 0x400
+        }
+
+    }
+}
